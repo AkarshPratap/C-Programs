bb647ad3b547ad288a656d10f6b3a04d127f4fe2
unknown <akarshpratap3107@gmail.com>
2022-03-07 00:57:01 +0700

    optimised binary search

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-
-    int n;
-    cin>>n;
-    int ans = 0; 
-    int i = 0;
-
-    while(n!=0){
-        int bit = n & 1;
-        ans = ( bit * powf(10,i) ) + ans;
-        n = n >> 1;
-        i++;
-    }
-    cout<< "Anaswer is " << ans << endl;
-}
+/2));
